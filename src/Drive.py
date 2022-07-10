e60bea85b0f33b4af848c05ccee6c438effd5841
Mohsen Hariri <mhariri68@gmail.com>
2022-07-10 00:38:42 -0400

    metadata

--- src/Drive.py
+++ src/Drive.py
@@ -2,7 +2,6 @@
 DocString
 https://docs.python.org/3/library/io.html
 """
-import json
 import pickle
 from os import getenv
 from pathlib import Path
@@ -14,6 +13,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+
+import mime
 
 
 class Drive:
@@ -51,164 +52,25 @@ class Drive:
             print("Can not stablish the connection.", err)
 
     def id_parser(self, url):
-        parse = urlparse(url).path.split("/")
-        type = parse[1]
-        id = parse[3]
-        return type, id
+        """
+        type =
+        drive
+        file
+        document, spreadsheet
+        """
+        # check the url is valid
+        try:
+            parse = urlparse(url).path.split("/")
+            type = parse[1]
+            id = parse[3]
+            return type, id
+        except Exception as err:
+            print("URL is invalid.")
+            exit(1)
 
     def sanitizer(self, name: str) -> str:
         file_name = name.replace("/", "_")  # replace illegal characters
         return file_name
-
-    def files_list(self):
-        results = (
-            self.service.files().list(pageSize=100, fields="files(id, name)").execute()
-        )
-        items = results.get("files", [])
-
-        # print a list of files
-
-        print("Here's a list of files: \n")
-        print(*items, sep="\n", end="\n\n")
-
-    def download_file(self, file_id, file_name):
-
-        request = self.service.files().get_media(fileId=file_id)
-
-        with open(f"./download/{file_name}", "wb") as fd:
-            downloader = MediaIoBaseDownload(fd, request)
-            print(downloader.__dict__)
-            done = False
-            while done is False:
-                status, done = downloader.next_chunk()
-                print("Download %d%%." % int(status.progress() * 100))
-
-    def download_google_files(self, url, export):
-        # https://developers.google.com/drive/api/guides/folder
-        _, id = self.id_parser(url)
-        mimeType = None
-        if export == "csv":
-            mimeType = "text/csv"
-        if export == "pdf":
-            mimeType = "application/pdf"
-
-        # print(id)
-        # mimeTypeMatchup = {
-        #     "application/vnd.google-apps.document": {
-        #         "exportType": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        #         "docExt": "docx",
-        #     }
-        # }
-        # res = self.service.files().get(fileId=id).execute()
-        # print(res)
-        try:
-            # request = self.service.files().export(fileId=id, mimeType="application/pdf")
-            request_metadata = self.service.files().get(fileId=id).execute()
-
-            name = self.sanitizer(request_metadata["name"])
-
-            request = self.service.files().export(fileId=id, mimeType=mimeType)
-
-            # exit()
-            with open(f"./download/{name}.{export}", "wb") as fd:
-                downloader = MediaIoBaseDownload(fd, request)
-                # print(downloader.__dict__)
-                # print(request.__dict__)
-
-                done = False
-                while done is False:
-                    status, done = downloader.next_chunk()
-                    print("Download %d%%." % int(status.progress() * 100))
-
-            # exportMimeType =mimeTypeMatchup[docMimeType]['exportType']
-
-            # request = self.service.files().export_media(fileId=id,mimeType=exportMimeType)
-        except HttpError as error:
-            print(f"An error occurred: {error}")
-        except Exception as error:
-            print(f"An Exception occurred: {error}")
-
-    def upload_file():
-        pass
-
-    def directory_structure(self, url):  # fin
-        """
-        finish
-
-        all files and folders in the given url
-        """
-
-        _, id = self.id_parser(url)
-
-        def traverse(id, path_parent):
-
-            files_and_folders = self.files_folder(id)
-
-            for file in files_and_folders:
-
-                path_directory = Path(rf'{path_parent}/{file["name"]}')
-                file["path"] = str(path_directory)
-
-                if file["mimeType"] == "application/vnd.google-apps.folder":
-                    file["inside"] = traverse(id=file["id"], path_parent=path_directory)
-
-            with open("save.json", "w") as fp:
-                json.dump(files_and_folders, fp)
-            return files_and_folders
-
-        return traverse(id, path_parent="./")
-
-    def down_all(self, url):
-
-        """
-        Finish
-        Download all files in parent and all sub directories
-        """
-        path_download = Path(getenv("PATH_DOWNLOAD"))
-        if not path_download.exists():
-            raise Exception("Download path doesn't exist.")
-
-        _, id = self.id_parser(url)
-
-        def traverse(id, path_parent):
-
-            files_and_folders = self.files_folder(id)
-
-            for file in files_and_folders:
-                if file["mimeType"] == "application/vnd.google-apps.folder":
-
-                    path_directory = Path(rf'{path_parent}/{file["name"]}')
-                    path_directory.mkdir(parents=True, exist_ok=True)
-                    file["inside"] = traverse(id=file["id"], path_parent=path_directory)
-
-                else:
-                    self.down_file(file, path_parent)
-
-            return files_and_folders
-
-        return traverse(id, path_download)
-
-    def down_file(self, item, path_parent):
-        """
-        finish
-        """
-        file_id = item["id"]
-
-        request = self.service.files().get_media(fileId=file_id)
-
-        file_name = item["name"].replace("/", "_")  # replace illegal characters
-
-        path_file = Path(rf"{path_parent}/{file_name}")
-
-        if path_file.exists():
-            print(f"{path_file} already exists.")
-
-        with open(path_file, "wb") as fd:
-            downloader = MediaIoBaseDownload(fd, request)
-            done = False
-            while done is False:
-                status, done = downloader.next_chunk()
-                print(f"{path_file} is downloading {int(status.progress() * 100)}")
 
     def files_folder(self, folder_id, pageSize=10):
 
@@ -235,7 +97,7 @@ class Drive:
             items = response.get("files", [])
             page_token = response.get("nextPageToken", None)
 
-            print("next")
+            # print("next")
 
             if page_token is None:
                 break
@@ -245,42 +107,80 @@ class Drive:
 
         return items
 
-    def download_folder(self, folder_id):
-        page_token = None
-        while True:
-            # Call the Drive v3 API
-            results = (
-                self.service.files()
-                .list(
-                    # q=f"'{folder_id}' in parents",
-                    pageSize=30,
-                    # supportsAllDrives = True,
-                    fields="nextPageToken, files(id, name)",
-                    pageToken=page_token,
-                    # driveId =  "0AIyERKEAMNpHUk9PVA",
-                    fileId=file_id,
-                )
-                .execute()
-            )
-            items = results.get("files", [])
+    def file_metadata(self, file_id, out):
+        metadata = self.service.files().get(fileId=file_id).execute()
+        item = metadata
+        self.download_file(item, out)
 
-            if not items:
-                print("No files found.")
+    def download_file(self, item, path_parent):
+        """ """
+        file_mime = item["mimeType"]
+        file_id = item["id"]
+        file_name = self.sanitizer(item["name"])
+
+        try:
+            if file_mime in mime.google:
+                mime_convert = {
+                    "application/vnd.google-apps.spreadsheet": "application/pdf",
+                    "application/vnd.google-apps.document": "application/pdf",
+                }
+                mimeType = mime_convert[file_mime]
+                extension = "pdf"
+                file_name = f"{file_name}.{extension}"
+
+                request = self.service.files().export(fileId=file_id, mimeType=mimeType)
+
             else:
-                for item in items:
-                    print("{0} ({1})".format(item["name"], item["id"]))
+                request = self.service.files().get_media(fileId=file_id)
 
-                    file_id = item["id"]
-                    request = self.service.files().get_media(fileId=file_id)
+            path_file = Path(rf"{path_parent}/{file_name}")
 
-                    with open(item["name"], "wb") as fd:
-                        downloader = MediaIoBaseDownload(fd, request)
+            if path_file.exists():
+                print(f"{path_file} already exists.")
 
-                        done = False
-                        while done is False:
-                            status, done = downloader.next_chunk()
-                            print("Download %d%%." % int(status.progress() * 100))
+            with open(path_file, "wb") as fd:
+                downloader = MediaIoBaseDownload(fd, request)
+                done = False
+                while done is False:
+                    status, done = downloader.next_chunk()
+                    print(f"{path_file} is downloading {int(status.progress() * 100)}")
 
-            page_token = results.get("nextPageToken", None)
-            if page_token is None:
-                break
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+        except Exception as error:
+            print(f"An Exception occurred: {error}")
+
+    def download(self, url, depth, out):
+        """
+        Download all files in parent and all sub directories
+        """
+        type, id = self.id_parser(url)
+
+        def traverse(id, path_parent, depth_current):
+            if depth_current > depth:
+                print("finish depth")
+                return
+
+            files_and_folders = self.files_folder(id)
+
+            for file in files_and_folders:
+                mime_type = file["mimeType"]
+
+                if mime_type == "application/vnd.google-apps.folder":
+
+                    path_directory = Path(rf'{path_parent}/{file["name"]}')
+                    path_directory.mkdir(parents=True, exist_ok=True)
+                    file["inside"] = traverse(
+                        id=file["id"],
+                        path_parent=path_directory,
+                        depth_current=depth_current + 1,
+                    )
+                else:
+                    self.download_file(file, path_parent)
+
+            return files_and_folders
+
+        if type == "drive":
+            return traverse(id, out, depth_current=0)
+        else:
+            self.file_metadata(id, out)
--- src/app.py
+++ src/app.py
@@ -1,75 +1,38 @@
-import argparse
-from lib2to3.pgen2.driver import Driver
+from argparse import ArgumentParser
+from os import getenv
+from pathlib import Path
 
 from Drive import Drive
 
-parser = argparse.ArgumentParser(description="URL of file or folder")
+path_download_required = False
+path_download = Path(getenv("PATH_DOWNLOAD"))
+if not path_download.exists():
+    path_download_required = True
 
-parser.add_argument("-url", type=str, default=None, metavar="N", help="URL")
-u = parser.parse_args().url
+parser = ArgumentParser(description="URL of file or folder")
 
+parser.add_argument("-u", "--url", help="URL", type=str, default=None, required=True)
+parser.add_argument("-d", "--depth-level", help="Depth", type=str, default="0")
+parser.add_argument(
+    "-o",
+    "--out",
+    help="Out",
+    type=str,
+    default=path_download,
+    required=path_download_required,
+)
+parser.add_argument("-mi", "--mimetype-include", help="Depth", type=str, default="all")
+parser.add_argument("-me", "--mimetype-exclude", help="Depth", type=str, default=None)
+
+
+args = parser.parse_args()
+url = args.url
+depth = int(args.depth_level)
+out = Path(args.out)
+
+if not out.exists():
+    out.mkdir(parents=True)
 
 drive = Drive()
 
-
-# print(
-#     drive.id_parser(
-#         "https://drive.google.com/drive/folders/1oEdQ6XNZ6wbn6dY98vFcf2ofOrY5GX-r?usp=sharing"
-#     )
-# )
-
-# print(
-#     drive.id_parser(
-#         "https://drive.google.com/file/d/1sRTNDHiowo7vhhxzg-8IvVTbVI96QDIz/view?usp=sharing"
-#     )
-# )
-
-# drive.files_list()
-# drive.download_file('19XDepgl7JF0VkSGfqpn64W_uwAw7tmJG','fsd.png')
-# print(drive.files_folder("1Eu2e4m3nH4Mwh8Jc6r_ULJ4U2y1nK6jK"))
-# print(
-#     drive.files_folder(
-#         "https://drive.google.com/drive/folders/1oEdQ6XNZ6wbn6dY98vFcf2ofOrY5GX-r"
-#     )
-# )
-
-# url =   "https://drive.google.com/drive/folders/1Eu2e4m3nH4Mwh8Jc6r_ULJ4U2y1nK6jK"
-# url = "https://drive.google.com/drive/folders/1Eu2e4m3nH4Mwh8Jc6r_ULJ4U2y1nK6jK"
-# url = "https://drive.google.com/drive/folders/0ABMmE9OGuOAaUk9PVA"
-# url = "https://drive.google.com/drive/folders/16oxBwnA9vchagtijZGtwjXSy0yni8C5r"
-# url = u
-
-# url = "https://drive.google.com/drive/folders/1Eu2e4m3nH4Mwh8Jc6r_ULJ4U2y1nK6jK"
-
-# url = "https://drive.google.com/drive/folders/10XArxkv1FwwHgPg-uWePXmDL8EqQMAAS"
-
-# drive.files_folder(url)
-
-# drive.download_folder(folder_id)
-
-url = "https://drive.google.com/drive/folders/1Eu2e4m3nH4Mwh8Jc6r_ULJ4U2y1nK6jK"
-
-
-# url = "https://drive.google.com/drive/folders/1xbMsmuiFXgNDC1YPX9-e61jUXLMSbroC"
-# url = "https://drive.google.com/drive/folders/118iMdI-X9S5o5-1sGrVK1P4_o6HzN4zr"
-
-
-# url = "https://drive.google.com/drive/folders/1k7ZJYxJQF9APBsFgP9Q2BHGk_Rjn4iPY"
-# drive.down_all(url)
-
-# print(drive.directory_structure(url))
-
-
-# drive.download_all(url)
-
-
-# url = "https://drive.google.com/drive/folders/10siyKtClyXCLAK3KVfbFHHL4RoCtjgK2"
-
-# print(drive.id_parser(url))
-# print(drive.directory_structure(url))
-
-url = "https://docs.google.com/spreadsheets/d/1P9HnkojMJnMRA---HfoEKrMH6bJyUCi7YXkXF_UhAWY/edit?usp=sharing"
-
-# print(drive.id_parser(url))
-drive.download_google_files(url, "pdf")
-drive.download_google_files(url, "csv")
+drive.download(url, depth, out)
